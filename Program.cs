ef40c2613d3795253865bd5fb33252d778a7de75
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-04-17 19:48:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using System.Threading;
+using System.Globalization;
+using HtmlAgilityPack;
+using Spectre.Console;
+
+string locale = Thread.CurrentThread.CurrentCulture.Name.ToLower();
+AnsiConsole.Markup($"[green]Grabbing Firmware...\n\nDetected Locale: {locale}[/]");
+await Task.Delay(5000);
+AnsiConsole.Clear();
+
+var client = new HttpClient();
+HtmlWeb w = new HtmlWeb();
+var hd = w.Load($"https://www.playstation.com/{locale}/support/hardware/ps4/system-software/");
+string[] ReinstallLinkAndText = new string[3];
+string[] NormalLinkAndText = new string[3];
+var stuff = hd.DocumentNode.SelectSingleNode(@"/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]
+");
+string ChangeLogs = " " + stuff.InnerText.TrimEnd();
+var linksOnPage = from lnks in hd.DocumentNode.Descendants()
+                  where lnks.Name == "a" &&
+                       lnks.Attributes["href"] != null &&
+                       lnks.InnerText.Trim().Length > 0
+                  select new
+                  {
+                      Url = lnks.Attributes["href"].Value,
+                      Text = lnks.InnerText
+                  };
+
+
+
+foreach (var i in linksOnPage)
+{
+
+    if (i.Url.StartsWith("https://pc.ps4.update.playstation.net") && i.Url.Contains("rec_"))
+    {
+        ReinstallLinkAndText[0] = i.Url;
+        ReinstallLinkAndText[1] = i.Text.Trim();
+        AnsiConsole.Markup("[Green]Grabbed Reinstallation Firmware![/]\n");
+    }
+    else if (i.Url.StartsWith("https://pc.ps4.update.playstation.net") && i.Url.Contains("sys_")) 
+    {
+        NormalLinkAndText[0] = i.Url;
+        NormalLinkAndText[1] = i.Text.Trim();
+        AnsiConsole.Markup("[Green]Grabbed Normal Update Firmware![/]\n");
+
+    }
+}
+
+AnsiConsole.Markup("[Green]Proceeding To Download Page...[/]");
+
+await Task.Delay(5000);
+
+AnsiConsole.Clear();
+
+var Choice = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("[Yellow]What Firmware Do You Need??[/]")
+        .PageSize(10)
+        .AddChoices(new[] {
+            "Full Firmware (Recovery)","Normal System Update"
+        }));
+
+async Task Download(HttpClient client, ProgressTask task, string url)
+{
+    try
+    {
+        using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+        {
+            response.EnsureSuccessStatusCode();
+
+            task.MaxValue(response.Content.Headers.ContentLength ?? 0);
+            task.StartTask();
+
+            var filename = @"Downloaded Firmware\" + url.Substring(url.LastIndexOf('/') + 1);
+            AnsiConsole.MarkupLine($"Starting download of [u]{filename.Replace(@"Downloaded Firmware\","")}[/] ({task.MaxValue} bytes)");
+
+            using (var contentStream = await response.Content.ReadAsStreamAsync())
+            using (var fileStream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+            {
+                var buffer = new byte[8192];
+                while (true)
+                {
+                    var read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+                    if (read == 0)
+                    {
+                        AnsiConsole.MarkupLine($"Download of [u]{filename.Replace(@"Downloaded Firmware\", "")}[/] [green]completed![/]");
+                        break;
+                    }
+
+                    task.Increment(read);
+
+                    await fileStream.WriteAsync(buffer, 0, read);
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        AnsiConsole.MarkupLine($"[red]Error:[/] {ex}");
+    }
+}
+
+
+switch (Choice)
+{
+    case "Full Firmware (Recovery)":
+        AnsiConsole.WriteLine(ChangeLogs);
+        AnsiConsole.Markup("[Green]Thats all for changelogs :)\nPress Any Key To Continue...[/]");
+        Console.ReadKey();
+        AnsiConsole.Clear();
+        Directory.CreateDirectory("Downloaded Firmware");
+        await AnsiConsole.Progress()
+    .Columns(new ProgressColumn[]
+    {
+        new TaskDescriptionColumn(),
+        new ProgressBarColumn(),
+        new PercentageColumn(),
+        new RemainingTimeColumn(),
+        new SpinnerColumn(),
+    })
+    .StartAsync(async ctx =>
+    {
+        var items = new (string name, string url)[]
+        {
+            ($"Full Recovery Firmware ({ChangeLogs.TrimStart().Split('\n').First()})",ReinstallLinkAndText[0])
+        };
+
+        await Task.WhenAll(items.Select(async item =>
+        {
+            var task = ctx.AddTask(item.name, new ProgressTaskSettings
+            {
+                AutoStart = false
+            });
+
+            await Download(client, task, item.url);
+        }));
+    });
+        break;  
+    case "Normal System Update":
+        AnsiConsole.WriteLine(ChangeLogs);
+        AnsiConsole.Markup("[Green]Thats all for changelogs :)\nPress Any Key To Continue...[/]");
+        Console.ReadKey();
+        Directory.CreateDirectory("Downloaded Firmware");
+        AnsiConsole.Clear();
+        await AnsiConsole.Progress()
+    .Columns(new ProgressColumn[]
+    {
+        new TaskDescriptionColumn(),
+        new ProgressBarColumn(),
+        new PercentageColumn(),
+        new RemainingTimeColumn(),
+        new SpinnerColumn(),
+    })
+    .StartAsync(async ctx =>
+    {
+        var items = new (string name, string url)[]
+        {
+            ($"Normal Firmware ({ChangeLogs.TrimStart().Split('\n').First()})",NormalLinkAndText[0])
+        };
+
+        await Task.WhenAll(items.Select(async item =>
+        {
+            var task = ctx.AddTask(item.name, new ProgressTaskSettings
+            {
+                AutoStart = false
+            });
+
+            await Download(client, task, item.url);
+        }));
+    });
+
+        break;
+}
+
+Thread.Sleep(-1);
