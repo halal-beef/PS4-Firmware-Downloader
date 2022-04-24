a5ae1fd7974d46d96afbd9b01d153477ac41058f
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-04-24 15:43:40 +0100

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -10,14 +10,17 @@ dotnet publish "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloade
 
 dotnet publish "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/PS4 Firmware Downloader.csproj" /p:DefineConstants=LINUX --output "build-debug/" --arch x64 --os linux -c debug --self-contained true # linux build
 
-dotnet publish "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/PS4 Firmware Downloader.csproj" /p:DefineConstants=LINUX --output "build/" --arch x64 --os macos -c release --self-contained true # MacOS build
+dotnet publish "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/PS4 Firmware Downloader.csproj" /p:DefineConstants=OSX --output "build-osx/" --arch x64 --os osx -c release --self-contained true # MacOS build
 
-dotnet publish "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/PS4 Firmware Downloader.csproj" /p:DefineConstants=LINUX --output "build-debug/" --arch x64 --os macos -c debug --self-contained true # MacOS build
+dotnet publish "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/PS4 Firmware Downloader.csproj" /p:DefineConstants=OSX --output "build-debug-osx/" --arch x64 --os osx -c debug --self-contained true # MacOS build
 
 # Move debug build
 mv "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build-debug/PS4 Firmware Downloader.exe" "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build/DEBUG-PS4 Firmware Downloader.exe"
 
-# Move debug build
 mv "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build-debug/PS4 Firmware Downloader" "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build/Linux-DEBUG-PS4 Firmware Downloader"
 
 mv "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build/PS4 Firmware Downloader" "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build/Linux-PS4 Firmware Downloader"
+
+mv "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build-debug-osx/PS4 Firmware Downloader" "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build/MacOS-DEBUG-PS4 Firmware Downloader"
+
+mv "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build-osx/PS4 Firmware Downloader" "/home/runner/work/PS4-Firmware-Downloader/PS4-Firmware-Downloader/build/MacOS-PS4 Firmware Downloader"
